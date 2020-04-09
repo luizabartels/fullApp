eeaa2959cbea519d745751bbbaebc6bccae4b022
luizabartels <luiza.bartels@uem.com.br>
2020-04-09 20:30:11 -0300

    upload - 09.04.2020

--- app.js
+++ app.js
@@ -1,31 +1,56 @@
 //Loading modules
     const express = require('express')
-    const Handlebars = require('handlebars')
     const handlebars = require('express-handlebars')
+    const Handlebars = require('handlebars')
     const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
-    const insecureHandlebars = allowInsecurePrototypeAccess(Handlebars)
-    
     const bodyParser = require('body-parser')
     const mongoose = require('mongoose')
-
     const app = express()
-
+    const admin = require("./routes/admin")
+    const path = require('path')
+    const session = require("express-session")
+    const flash = require("connect-flash")
 // Configurations
+    //Session
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    //Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next() // Tem que colocar o next, caso contrário ele para a aplicação aqui. Em cada página nova o middleware é chamado.
+        })
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
     //Handlebars
-        app.engine('handlebars', handlebars({defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars)}))
+        app.engine('handlebars', handlebars({defaultLayout: 'main', 
+                                             handlebars: allowInsecurePrototypeAccess(Handlebars)} ))
         app.set('view engine', 'handlebars')
+
+
     //Mongoose
+        mongoose.Promise = global.Promise
+        mongoose.connect('mongodb://localhost/blogApp', {useUnifiedTopology: true, useNewUrlParser: true,
+        }).then(() => {
+            console.log("MongoDB conectado")
+        }).catch((err) => {
+            console.log("Houve um erro ao se conectar ao mongoDB: "+err)
+        }) 
+    //Public
+        app.use(express.static(path.join(__dirname, "/public")))
 
 //Routes
+    app.use('/admin', admin)
+
 
 //Others
 
-const PORT = 8081
-app.listen(PORT, () => {
-    console.log("Servidor rodando em http://localhost:8081")
-}).catch((err) => {
-    console.log("Erro ao conectar com o servidor.")
-})
+    const PORT = 8081
+    app.listen(PORT, () => {
+        console.log("Servidor rodando em http://localhost:8081")
+    })
---
+++ routes/admin.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
+
+router.get('/', (req, res) => {
+    res.render('admin/index')
+})
+
+router.get('/posts', (req, res) => {
+    res.send("Página de posts")
+})
+
+router.get('/categorias', (req, res) => {
+    Categoria.find().sort({date:'desc'}).then((categorias) => {
+        res.render('admin/categorias', {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias")
+        res.redirect('/admin')
+    })
+    
+})
+
+router.get('/categorias/add', (req, res) => {
+    res.render('admin/addcategorias')
+})
+
+router.post('/categorias/nova', (req, res) => {
+
+    var erros = []
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null)
+    {
+        erros.push({texto: "Nome inválido"})
+    }
+
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null)
+    {
+        erros.push({texto: "Slug inválido"})
+    }
+
+    if (req.body.nome.length < 2)
+    {
+        erros.push({texto: "Nome da categoria é menor que dois caractéres!"})
+    }
+
+    if (erros.length > 0)
+    {
+        res.render('admin/addcategorias', {erros: erros})
+    } 
+    else
+    {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash("success_msg", "Categoria criada com sucesso!")
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!")
+            res.redirect('/admin')
+        })
+    }
+})
+
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id:req.params.id}).then((categoria) => {
+        res.render('admin/editcategorias', {categoria: categoria})
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe")
+        res.redirect('/admin/categorias')
+    })
+})
+
+router.post('/categorias/edit', (req, res) => {
+   Categoria.findOne({_id: req.body.id}).then((categoria) => {
+    
+    categoria.nome = req.body.nome
+    categoria.slug = req.body.slug
+
+    categoria.save().then(() => {
+        req.flash("success_msg", "Categoria editada com sucesso!")
+        res.redirect('/admin/categorias')
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao salvar edição de categoria")
+        res.redirect('/admin/categorias')
+       })
+
+
+   }).catch((err) => {
+    req.flash("error_msg", "Houve um erro ao editar a categoria")
+    res.redirect('/admin/categorias')
+   })
+})
+
+module.exports = router
