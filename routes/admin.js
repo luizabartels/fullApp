e9e68ccd66d793f4ce714d731977e0b66dcce124
luizabartels <luiza.bartels@uem.com.br>
2020-04-12 21:04:29 -0300

    upload - 12.04.2020

--- routes/admin.js
+++ routes/admin.js
@@ -96,4 +96,14 @@ router.post('/categorias/edit', (req, res) => {
    })
 })
 
+router.post('/categorias/deletar', (req, res) => {
+     Categoria.remove({_id: req.body.id}).then(() => {
+         req.flash("success_msg", "Categoria deletada com sucesso!")
+         res.redirect('/admin/categorias')
+     }).catch((err) => {
+         req.flash("error_msg", "Houve um erro ao deletar categoria")
+         res.redirect('/admin/categorias')
+        })
+ })
+
 module.exports = router
